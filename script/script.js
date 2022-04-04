9b28ee23108c3276d67e6b1e4b75005c28a30a93
Pedro Tomaz <pedrohtomaz2021@gmail.com>
2022-04-04 16:36:14 -0300

    function

--- script/script.js
+++ script/script.js
@@ -1,3 +1,8 @@
 let vconvertidor = document.getElementById("valorAserConvertido")
 let dolaratual = 4.62
 let res = vconvertidor / dolaratual
+
+function converter () {
+    getElementById('bconverter') = res
+}
+console.log(`O valor em dolar fica ${res}`)
