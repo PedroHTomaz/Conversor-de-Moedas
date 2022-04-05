4f71792cb2b40bc82b35844183645388ae42f355
Pedro Tomaz <pedrohtomaz2021@gmail.com>
2022-04-05 18:18:52 -0300

    Projeto ganhou forma!

--- script/script.js
+++ script/script.js
@@ -1,8 +1,11 @@
-function converter () {
-    var vconvertidor = document.getElementById("valorAserConvertido").value
-    var dolaratual = 4.66
-    var res = vconvertidor / dolaratual
-    var mostrar = document.getElementById("exit")
 
-    mostrar.innerHTML = res
+function converter(){
+
+    let vconvertidor = document.getElementById("valorAserConvertido").value
+    let dolaratual = 4.65 //Cotação do dia 05.04.2022
+    let res = vconvertidor / dolaratual
+    let mostrar = document.getElementById("exit")
+
+    mostrar.innerHTML = `$ ${res.toFixed(2)}`
+
 }
