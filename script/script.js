24543696ba8c4ef1cc6b4903e3efb1af638e3c13
Pedro Tomaz <pedrohtomaz2021@gmail.com>
2022-04-05 15:45:21 -0300

    Código inicial funcional

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
-var vconvertidor = document.getElementById("valorAserConvertido").value
-var dolaratual = 4.66
-var res = vconvertidor / dolaratual
-
 function converter () {
-    document.getElementById("exit") = 
+    var vconvertidor = document.getElementById("valorAserConvertido").value
+    var dolaratual = 4.66
+    var res = vconvertidor / dolaratual
+    var mostrar = document.getElementById("exit")
+
+    mostrar.innerHTML = res
 }
